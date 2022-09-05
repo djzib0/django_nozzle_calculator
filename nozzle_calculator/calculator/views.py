98bac29ef0a6e66abdadfadc69757ab689a4b5a0
piotr <piotrsabatura@gmail.com>
2022-09-05 22:25:29 +0200

    added nozzle_total_orders variable and implemented in template view

--- nozzle_calculator/calculator/views.py
+++ nozzle_calculator/calculator/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Nozzle
+from .models import Nozzle, Order
 
 from .functions import translate_type_name
 
@@ -24,9 +24,13 @@ def nozzleDetailsView(request, nozzle_id):
     nozzle = Nozzle.objects.get(id=nozzle_id)
     ratio = round((nozzle.profile_height / nozzle.diameter), 1)
     type_name = translate_type_name(str(nozzle.inner_ring_type))
+    orders = Order.objects.all()
+    nozzle_total_orders = orders.filter(nozzle=nozzle).count()
+
     context = {'nozzle': nozzle,
                'ratio': ratio,
                'type_name': type_name,
+               'nozzle_total_orders': nozzle_total_orders,
                }
     template = 'calculator/nozzle-details.html'
 
