6c3b7917919d41d159385b78b62a8d4cb555beea
piotr <piotrsabatura@gmail.com>
2022-09-22 20:05:25 +0200

    adjusted styles and templates

--- nozzle_calculator/calculator/forms.py
+++ nozzle_calculator/calculator/forms.py
@@ -3,6 +3,29 @@ from django import forms
 from .models import Nozzle, Order
 
 class NozzleForm(forms.ModelForm):
+    PROFILES = (('Optima', 'Optima'),
+                ('19A', '19A'),
+                )
+
+    INNER_RING_TYPES = (('Complete st. st. inside', 'Wnętrze nierdzewne'),
+                        ('St. st. ring inside', 'Pierścień kawitacyjny nierdzewny'),
+                        ('Complete steel', 'Wnętrze ze stali zwykłej'),
+                        ('St. st. ring and outlet', 'Pierścień kawitacyjny i wylot ze stali nierdzewnej'))
+
+    YES_NO_CHOICES = (('True', 'Tak'),
+                      ('False', 'Nie')
+                      )
+
+    profile = forms.ChoiceField(label="test label",
+                                choices=PROFILES,
+                                widget=forms.Select(attrs={"class": "wide_form"}))
+    inner_ring_type = forms.ChoiceField(label="test label",
+                                                  choices=INNER_RING_TYPES,
+                                                  widget=forms.Select(attrs={"class": "wide_form"}))
+    has_headbox = forms.ChoiceField(choices=YES_NO_CHOICES,
+                                              widget=forms.Select(attrs={"class": "wide_form"}))
+    has_outlet_ring = forms.ChoiceField(choices=YES_NO_CHOICES,
+                                                  widget=forms.Select(attrs={"class": "wide_form"}))
     class Meta:
         model = Nozzle
         fields = '__all__'
@@ -21,6 +44,8 @@ class NozzleForm(forms.ModelForm):
                   'segments_thickness': 'Grubość półek',
                   'has_headbox': 'Dysza ze skrzynią?',
                   'has_outlet_ring': 'Dysza z pierścieniem wlotowym?'}
+
+
 
 
 class OrderForm(forms.ModelForm):
